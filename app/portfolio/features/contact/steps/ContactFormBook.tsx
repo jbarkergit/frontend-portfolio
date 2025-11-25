04e15baf16079e36e8bbb6216f538aec05eb036b
Justin Barker <jbarkerpoc@gmail.com>
2025-11-24 23:03:48 -0600

    wip(contact): booking calendar

--- app/portfolio/features/contact/steps/ContactFormBook.tsx
+++ app/portfolio/features/contact/steps/ContactFormBook.tsx
@@ -1,8 +1,12 @@
-import type { JSX } from 'react';
+import { useMemo, useState, type JSX } from 'react';
 import {
   MaterialSymbolsCircle,
   MaterialSymbolsArrowShapeUpStack2,
+  MaterialSymbolsArrowLeftAlt,
+  MaterialSymbolsArrowRightAlt,
 } from '~/portfolio/features/contact/assets/ContactFormSVG';
+
+const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'] as const;
 
 const ContactFormBook = ({
   errors,
@@ -13,6 +17,43 @@ const ContactFormBook = ({
   ProjectHubBtn: () => JSX.Element;
   PreviousStepBtn: () => JSX.Element;
 }) => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  // Dates
+  const dates = useMemo(
+    () => ({
+      today: new Date(),
+      year: currentDate.getFullYear(),
+      month: currentDate.getMonth(),
+    }),
+    [currentDate]
+  );
+
+  const { today, year, month } = dates;
+
+  // Helpers
+  const prevMonth = () => setCurrentDate(new Date(year, month - 1, 1));
+  const nextMonth = () => setCurrentDate(new Date(year, month + 1, 1));
+  const isToday = (date: Date) => {
+    return (
+      date.getFullYear() === today.getFullYear() &&
+      date.getMonth() === today.getMonth() &&
+      date.getDate() === today.getDate()
+    );
+  };
+
+  // Map suppliers
+  const firstDayOfMonth = new Date(year, month, 1);
+  const lastDayOfMonth = new Date(year, month + 1, 0);
+
+  const maps = useMemo(
+    () => ({
+      daysInMonth: Array.from({ length: lastDayOfMonth.getDate() }, (_, i) => new Date(year, month, i + 1)),
+      emptyDays: Array.from({ length: firstDayOfMonth.getDay() }, () => null),
+    }),
+    [lastDayOfMonth]
+  );
+
   return (
     <section
       className='contact__form__step'
@@ -27,10 +68,57 @@ const ContactFormBook = ({
         </div>
         <h2>Let's book a call.</h2>
       </header>
-      <ul className='contact__form__step__ul'></ul>
+
+      <div className='contact__form__step__calendar'>
+        <nav className='contact__form__step__calendar__head'>
+          <button
+            aria-label='Previous Month'
+            type='button'
+            onClick={prevMonth}>
+            <MaterialSymbolsArrowLeftAlt />
+          </button>
+          <div>{currentDate.toLocaleString('default', { month: 'long', year: 'numeric' })}</div>
+          <button
+            aria-label='Next Month'
+            type='button'
+            onClick={nextMonth}>
+            <MaterialSymbolsArrowRightAlt />
+          </button>
+        </nav>
+        <ul className='contact__form__step__calendar__weekdays'>
+          {weekdays.map((day) => (
+            <li
+              className='contact__form__step__calendar__weekdays__day'
+              key={`calendar-day-${day}`}>
+              {day}
+            </li>
+          ))}
+        </ul>
+        <ul className='contact__form__step__calendar__selection'>
+          {maps.emptyDays.map((_, i) => (
+            <li
+              className='contact__form__step__calendar__selection__opt'
+              key={`calendar-empty-day-${i}`}
+            />
+          ))}
+          {maps.daysInMonth.map((date) => (
+            <li
+              className='contact__form__step__calendar__selection__opt'
+              key={`calendar-date-${date.toDateString()}`}>
+              <button
+                aria-label={`Select ${date}`}
+                type='button'
+                style={{ backgroundColor: isToday(date) ? '#4f46e5' : undefined }}>
+                {date.getDate()}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
       <nav className='contact__form__step__stepper'>
-        <div className='contact__form__step__stepper__section'>{PreviousStepBtn()}</div>
         <div className='contact__form__step__stepper__section'>
+          {PreviousStepBtn()}{' '}
           <button
             className='contact__form__step__stepper__section__button'
             aria-label='Submit form'
