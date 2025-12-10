147bd8232c81bd4148868b73ea0dff80aa352a31
Justin Barker <jbarkerpoc@gmail.com>
2025-12-10 15:48:01 -0600

    build: vercel support @vercel/react-router/vite

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from '@react-router/dev/config';
+import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
   ssr: false,
+  presets: [vercelPreset()],
   prerender: ['/'],
 } satisfies Config;
