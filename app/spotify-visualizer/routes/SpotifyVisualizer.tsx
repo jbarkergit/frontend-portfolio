730f7629e00d3c22d224f3782f9ca63ba6a5a032
Justin Barker <jbarkerpoc@gmail.com>
2025-12-17 16:55:16 -0600

    chore(spotifyVisualizer): remove auth on prod load for wip project

--- app/spotify-visualizer/routes/SpotifyVisualizer.tsx
+++ app/spotify-visualizer/routes/SpotifyVisualizer.tsx
@@ -1,26 +1,26 @@
-import { initSpotifyAuth } from '~/spotify-visualizer/composables/auth/SpotifyAuthFlow';
-import Controller from '~/spotify-visualizer/features/controller/Controller';
-import Main from '~/spotify-visualizer/features/main/Main';
-import Navigation from '~/spotify-visualizer/features/navigation/Navigation';
-import Sidebar from '~/spotify-visualizer/features/sidebar/Sidebar';
+// import { initSpotifyAuth } from '~/spotify-visualizer/composables/auth/SpotifyAuthFlow';
+// import Controller from '~/spotify-visualizer/features/controller/Controller';
+// import Main from '~/spotify-visualizer/features/main/Main';
+// import Navigation from '~/spotify-visualizer/features/navigation/Navigation';
+// import Sidebar from '~/spotify-visualizer/features/sidebar/Sidebar';
 
-export async function clientLoader() {
-  // void (await initSpotifyAuth());
-}
+// export async function clientLoader() {
+//   // void (await initSpotifyAuth());
+// }
 
-// export const useSpotifyLoader = () => useLoaderData() as Awaited<ReturnType<typeof clientLoader>>;
+// // export const useSpotifyLoader = () => useLoaderData() as Awaited<ReturnType<typeof clientLoader>>;
 
-export default function SpotifyVisualizer() {
-  return (
-    <div className='spotify'>
-      <div className='spotify__wrapper'>
-        <div className='spotify__wrapper__content'>
-          <Navigation />
-          <Sidebar />
-          <Main />
-          <Controller />
-        </div>
-      </div>
-    </div>
-  );
-}
+// export default function SpotifyVisualizer() {
+//   return (
+//     <div className='spotify'>
+//       <div className='spotify__wrapper'>
+//         <div className='spotify__wrapper__content'>
+//           <Navigation />
+//           <Sidebar />
+//           <Main />
+//           <Controller />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
