8cbe7231c286209038ec2b7d883e1601fa982ffe
= <jbarkerpoc@gmail.com>
2026-01-13 15:37:38 -0600

    build: improved performance and build size

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,52 @@
+/// <reference types="vitest/config" />
+
 import path from 'node:path';
 import { reactRouter } from '@react-router/dev/vite';
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
+import { defineConfig } from 'vite';
+import type { UserConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import type { ViteUserConfig } from 'vitest/config';
-import { defineConfig } from 'vitest/config';
 
-/** https://vite.dev/config/ */
 export default defineConfig({
-  root: './',
+  root: './app',
+  // https://www.npmjs.com/package/vite-tsconfig-paths
+  // https://www.npmjs.com/package/@react-router/dev
+  plugins: [tsconfigPaths(), reactRouter()],
+  publicDir: false,
   resolve: {
     alias: {
       app: path.resolve(__dirname, 'app'),
     },
   },
-  plugins: [tsconfigPaths(), ...(process.env['VITEST'] ? [] : [reactRouter()])],
+  // https://lightningcss.dev/docs.html
+  css: {
+    transformer: 'lightningcss',
+    // https://github.com/parcel-bundler/lightningcss/blob/master/node/index.d.ts
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('>= 0.25%')),
+    },
+  },
+  json: {
+    stringify: true,
+  },
+  clearScreen: false,
+  appType: 'spa',
   server: {
     host: '127.0.0.1',
+    port: 5173,
     watch: { usePolling: true },
   },
+  build: {
+    cssMinify: 'lightningcss',
+    minify: 'esbuild',
+    copyPublicDir: false,
+  },
+  // https://vitest.dev/config/
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./setupTests.ts'],
     include: ['**/*.test.{ts,tsx}'],
   },
-  build: {
-    copyPublicDir: false,
-    cssMinify: true,
-    ssr: false,
-  },
-}) satisfies ViteUserConfig;
+}) satisfies UserConfig;
